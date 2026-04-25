924b799d5d8436c3903c908cff947eae9aca61c4
Magnus <magnus@magkari.eu>
2026-04-25 22:11:27 +0000

    those who update

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,17 @@
 import tinyCSS from "@sardine/eleventy-plugin-tinycss";
 import pluginIcons from "eleventy-plugin-icons";
+import faviconPlugin from "eleventy-favicon";
+import safeLinks from "@sardine/eleventy-plugin-external-links";
+import poison from "eleventy-plugin-poison";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPlugin(tinyCSS);
+  eleventyConfig.addPlugin(faviconPlugin);
+  eleventyConfig.addPlugin(safeLinks);
+  eleventyConfig.addPlugin(poison, {
+    includeCSS: true,
+  });
   eleventyConfig.addPlugin(pluginIcons, {
     sources: [
       { name: "simple-icons", path: "node_modules/simple-icons/icons" },
